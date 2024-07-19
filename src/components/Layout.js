11df3d5071894b874fb701cd6d59dda14eb3f65d
FarazAhmad2 <farazahmad574@gmail.com>
2024-07-19 17:55:47 +0530

    third commit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,26 +4,26 @@ import { Link, Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <main>
-    <div>
-      <p>Women Items:</p>
-      <nav>
-        <ul>
-          <li>
-            <Link to="grooming">Grooming</Link>
-          </li>
-          <li>
-            <Link to="shirt">Shirt</Link>
-          </li>
-          <li>
-            <Link to="trouser">Trouser</Link>
-          </li>
-          <li>
-            <Link to="jewellery">Jewellery</Link>
-          </li>
-        </ul>
-      </nav>
-      <Outlet />  
-    </div>
+      <div>
+        <p>Women Items:</p>
+        <nav>
+          <ul>
+            <li>
+              <Link to="Grooming">Grooming</Link>
+            </li>
+            <li>
+              <Link to="Shirt">Shirt</Link>
+            </li>
+            <li>
+              <Link to="Trouser">Trouser</Link>
+            </li>
+            <li>
+              <Link to="Jewellery">Jewellery</Link>
+            </li>
+          </ul>
+        </nav>
+        <Outlet />
+      </div>
     </main>
   );
 };
